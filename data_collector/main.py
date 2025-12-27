ef18fccca15c60b33461ee032c95e282e7f0f06b
macbookair <macbookair@Macboo-Pro.local>
2025-12-27 20:33:45 +0100

    FIX: Cleanup code

--- data_collector/main.py
+++ data_collector/main.py
@@ -23,43 +23,49 @@ if not INFLUX_TOKEN:
 db_client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
 write_api = db_client.write_api(write_options=SYNCHRONOUS)
 
+
 def save_to_influx(data):
     try:
-        point = Point("machine_status") \
-            .tag("machine_id", data["machine_id"]) \
-            .field("temperature", float(data["temperature"])) \
-            .field("vibration", float(data["vibration"])) \
+        point = (
+            Point("machine_status")
+            .tag("machine_id", data["machine_id"])
+            .field("temperature", float(data["temperature"]))
+            .field("vibration", float(data["vibration"]))
             .field("status", data["status"])
+        )
 
         write_api.write(bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=point)
         print(f"✅ Saved to influx: {data['temperature']}°C")
-        
+
     except Exception as e:
         print(f"❌ Error to save to influx: {e}")
+
 
 def on_message(client, userdata, msg):
     try:
         payload_str = msg.payload.decode("utf-8")
-        
+
         data = json.loads(payload_str)
-        
+
         save_to_influx(data)
-        
+
     except Exception as e:
         print(f"Error: {e}")
 
-def run_collector():
 
+def run_collector():
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
-    client.on_connect = lambda c, u, f, rc, props: print("Data collector connected to MQTT")
+    client.on_connect = lambda c, u, f, rc, props: print(
+        "Data collector connected to MQTT"
+    )
     client.on_message = on_message
 
     while True:
         try:
             print(f"Connecting to MQTT {MQTT_BROKER}...")
             client.connect(MQTT_BROKER, 1883, 60)
-            break # Jak się uda, wyjdź z pętli while
+            break  # Jak się uda, wyjdź z pętli while
         except Exception as e:
             print(f" broker not available ({e}). waiting 5s...")
             time.sleep(5)
